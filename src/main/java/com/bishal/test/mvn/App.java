78c22a35637b9dd8637c9366d7895e14079b7abd
bishal9861 <bishalpatel9861@gmail.com>
2023-07-03 10:18:36 +0000

    add to repo

---
+++ src/main/java/com/bishal/test/mvn/App.java
@@ -0,0 +1,18 @@
+package com.bishal.test.mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Sport c=new Cricket();
+       Sport f=new Football();
+       Sport t=new Tennis();
+       c.startplay();
+       f.startplay();
+       t.startplay();
+    }
+}
